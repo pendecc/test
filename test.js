1a778d5cfb9b49427c4f667b62d6356eaebb7c99
pendecc <123860240+pendecc@users.noreply.github.com>
2023-01-29 14:36:10 +0700

    Update and rename test.html to test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<div style="height:30vh;width:100%;"></div>
+document.writeln='<div style="height:30vh;width:100%;"></div>
     <div style="font-family:'Courier New';display:block !important;position:fixed !important;left:0 !important;width:100% !important;bottom:0 !important;height:25vh !important;background:black;color:white;font-size:3.5vw;border-top:1vw solid tomato;overflow:auto;z-index:999999999 !important;">
       <h2 style="text-align:center;font-size:5vw;line-height:4vw;color:red;">
         <b>Website Tidak Aman!!</b>
@@ -13,4 +13,4 @@
         - <a href="mailto:kamupendecc@gmail.com?subject=Kamu Masuk BK!!&amp;body=Saya tunggu di ruang bk..." style="color: inherit">Muridmu</a>
       </p>
       <img style="display:block;width:30vw;margin:auto;" src="https://media.tenor.com/D9DtySO7uMcAAAAd/xelszy-pristel.gif" alt="hangker-pristel.gif">
-    </div>
+    </div>'
