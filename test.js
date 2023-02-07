332bd3438e9922d4d96b71f20f5105ee580e0ff5
pendecc <123860240+pendecc@users.noreply.github.com>
2023-02-07 14:19:00 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
-document.writeln='<div style="height:30vh;width:100%;"></div>
-    <div style="font-family:'Courier New';display:block !important;position:fixed !important;left:0 !important;width:100% !important;bottom:0 !important;height:25vh !important;background:black;color:white;font-size:3.5vw;border-top:1vw solid tomato;overflow:auto;z-index:999999999 !important;">
-      <h2 style="text-align:center;font-size:5vw;line-height:4vw;color:red;">
-        <b>Website Tidak Aman!!</b>
-      </h2>
-      <hr style="height:0.1vh;background:white;">
-      <p style="margin:0 4vw;text-align:justify;">
-        Sistem login admin pada website ini bisa di bypass dengan mudah.
-        <br />
-        <i>btw jangan lupa absensi ya sayy😚</i>
-      </p>
-      <p style="text-align:center;color:#4caf50;">
-        - <a href="mailto:kamupendecc@gmail.com?subject=Kamu Masuk BK!!&amp;body=Saya tunggu di ruang bk..." style="color: inherit">Muridmu</a>
-      </p>
-      <img style="display:block;width:30vw;margin:auto;" src="https://media.tenor.com/D9DtySO7uMcAAAAd/xelszy-pristel.gif" alt="hangker-pristel.gif">
-    </div>'
+<?php
+$output = shell_exec('ls -lart');
+echo "<pre>$output</pre>";
+?>
